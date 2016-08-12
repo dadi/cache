dc70be0d68dfc7b2d82acbb252e6b723d20d81c2
Adam K Dean <adamkdean@googlemail.com>
2016-08-12 17:35:37 +0100

    test: add redis integration tests

---
+++ test/integration/redis.js
@@ -0,0 +1,79 @@
+const _ = require('underscore')
+const fs = require('fs')
+const path = require('path')
+const should = require('should')
+const redis = require('ioredis')
+
+const Cache = require(__dirname + '/../../lib/index')
+const RedisCache = require(__dirname + '/../../lib/redis')
+
+var cache
+
+describe('RedisCache', function () {
+  // this.timeout(10000)
+
+  it('should connect to a redis non-clustered instance (127.0.0.1:6379)', (done) => {
+    cache = new Cache({
+      directory: {
+        enabled: false
+      },
+      redis: {
+        enabled: true,
+        cluster: false,
+        host: '127.0.0.1',
+        port: 6379
+      }
+    })
+
+    cache.cacheHandler.on('ready', () => {
+      done()
+    })
+  })
+
+  it('should connect to a redis cluster (127.0.0.1:7000-7002)', (done) => {
+    cache = new Cache({
+      directory: {
+        enabled: false
+      },
+      redis: {
+        enabled: true,
+        cluster: true,
+        hosts: [
+          { host: '127.0.0.1', port: 7000 },
+          { host: '127.0.0.1', port: 7001 },
+          { host: '127.0.0.1', port: 7002 }
+        ]
+      }
+    })
+
+    cache.cacheHandler.on('ready', () => {
+      done()
+    })
+  })
+
+  it('should write and read from a redis cluster (127.0.0.1:7000-7002)', (done) => {
+    cache = new Cache({
+      directory: {
+        enabled: false
+      },
+      redis: {
+        enabled: true,
+        cluster: true,
+        hosts: [
+          { host: '127.0.0.1', port: 7000 },
+          { host: '127.0.0.1', port: 7001 },
+          { host: '127.0.0.1', port: 7002 }
+        ]
+      }
+    })
+
+    cache.cacheHandler.on('ready', () => {
+      cache.set('foo', 'bar')
+      cache.get('foo').then((res) => {
+        res.should.eql('bar')
+        done()
+      })
+    })
+  })
+
+})
